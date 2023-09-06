bc124b6fa67fc219a3ebc01badc9703504ea1bd8
indisputable <tanmaykachroo6@outlook.com>
2023-09-07 00:04:53 +0530

    Async refactor

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,33 +1,35 @@
-import prisma from '@/lib/prisma'
+import prisma from "@/lib/prisma";
 import Dashboard from "./dashboard";
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export default async function QuizesPage() {
-    const session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions);
 
-    const quizes = await prisma.quiz.findMany({
-        include: {
-            results: {
-                where: {
-                    userId: session?.user.id
-                }
-                , include: {
-                    markedOptions: true
-                }
+    const [quizes, languages] = await Promise.all([
+        prisma.quiz.findMany({
+            include: {
+                results: {
+                    where: {
+                        userId: session?.user.id,
+                    },
+                    include: {
+                        markedOptions: true,
+                    },
+                },
+                language: true,
             },
-            language: true
-        }
-    });
+        }),
+        prisma.language.findMany(),
+    ]);
 
-    const languages = await prisma.language.findMany();
     return (
         <main className="flex flex-col items-center justify-between px-24">
-            <h1 className='mt-3 text-center text-4xl font-bold'>Take Quizes</h1>
-            <h2 className='mt-5 text-lg'>Explore set of available quizes to improve language proficiency</h2>
+            <h1 className="mt-3 text-center text-4xl font-bold">Take Quizes</h1>
+            <h2 className="mt-5 text-lg">
+                Explore set of available quizes to improve language proficiency
+            </h2>
             <Dashboard languages={languages} quizes={quizes} />
         </main>
-    )
+    );
 }
-
-
